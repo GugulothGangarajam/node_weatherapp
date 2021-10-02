587fbf788de5939de1d28993a2bb21b59acd24f1
GugulothGangarajam <76522749+GugulothGangarajam@users.noreply.github.com>
2021-10-03 00:47:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL:"http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "10215933ecc1a5002ebe94ab4e77330c"
+    }
+}
+
+module.exports =constants;
